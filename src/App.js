7a365623436b53bece107ec5f67e371f6bab7577
Xyz Fiegalan <xyzfiegalan@gmail.com>
2021-05-21 20:27:16 +0800

    fetch prices, promise.all

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import AccountBalance from './components/AccountBalance/AccountBalance';
 import CoinList from './components/CoinList/CoinList';
 import AppHeader from './components/AppHeader/AppHeader';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Div = styled.div`
   text-align: center;
   background-color: lightblue;
 `;
 
+const COIN_COUNT = 10;
+
 class App extends React.Component {
   state = {
     balance: 10000,
     showBalance: false,
     coinData: [
+      /*
       {
         name: 'Bitcoin',
         ticker: 'BTC',
@@ -38,8 +42,29 @@ class App extends React.Component {
         balance: 5,
         price: 600
       }
+      */
     ]
   }
+
+  componentDidMount = async () => {
+    const response = await axios.get('https://api.coinpaprika.com/v1/coins');
+    const coinIds = response.data.slice(0, COIN_COUNT).map(coin => coin.id);
+    const tickerUrl = 'https://api.coinpaprika.com/v1/tickers/';
+    const promises = coinIds.map(id => axios.get(tickerUrl + id)); //key will fetch the endpoints for each coin
+    const coinData = await Promise.all(promises);
+    const coinPriceData = coinData.map(function(response) {
+      const coin = response.data;
+      return {
+        key: coin.id,
+        name: coin.name,
+        ticker: coin.symbol,
+        balance: 0,
+        price: parseFloat(Number(coin.quotes.USD.price).toFixed(4))
+      }
+    })
+    this.setState({ coinData: coinPriceData })
+  }
+
 
   handleRefresh = (valueChangeTicker) => {
     const newCoinData = this.state.coinData.map( function ( values ) {
